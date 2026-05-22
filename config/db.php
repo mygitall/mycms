3239136cc071783118201f7ea6bc4e632b96e615
mygitall <zhangweiweiguge@gmail.com>
2026-05-22 11:50:49 +0800

    fix: CSP img-src 改为 https: 允许所有HTTPS图片

--- config/db.php
+++ config/db.php
@@ -1018,7 +1018,7 @@ function setSecurityHeaders() {
         "default-src 'self'",
         "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-        "img-src 'self' data: https://images.unsplash.com https://picsum.photos https://via.placeholder.com {$videoDomains}",
+        "img-src 'self' data: https:",
         "font-src 'self' https://fonts.gstatic.com",
         "connect-src 'self' {$videoDomains}",
         "media-src 'self' {$videoDomains}",
